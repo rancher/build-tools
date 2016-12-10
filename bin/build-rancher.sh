90aec6931b5902de0a40d2696c1593759c742c02
Darren Shepherd <darren@rancher.com>
2016-12-10 08:55:54 -0700

    Add build-rancher.sh

---
+++ bin/build-rancher.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+TAG=${TAG:-$(awk '/CATTLE_RANCHER_SERVER_VERSION/{print $3}' Dockerfile)}
+REPO=${REPO:-$(awk '/CATTLE_RANCHER_SERVER_IMAGE/{print $3}' Dockerfile)}
+IMAGE=${REPO}:${TAG}
+
+git tag -d $TAG || true
+git tag $TAG
+./build-image.sh
+git push origin
+git push origin $TAG
+docker push $IMAGE
